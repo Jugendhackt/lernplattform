25073f185d088ce5a4bb6381adfe311a3da548a4
bot196 <61917941+bot196@users.noreply.github.com>
2020-09-19 15:55:44 +0200

    file_upload

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!doctype html>
+<head>
 
-    
+</head>
+<body>
 
-?>
+    <?php
+        $target_dir = "./videos/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        }
+    ?>
+
+<form action="upload" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
